6582ea87f52d3a4661a0da1febaf144df97fe270
Praveensharma890 <psharma89090@gamil.com>
2026-03-28 21:09:35 +0530

    add type effect on prof

--- script.js
+++ script.js
@@ -16,3 +16,32 @@ theme_btn.addEventListener("click",()=>{
     num = 0;
     }
 })
+
+
+//title web developer prof
+let prof = document.querySelector(".my_prof");
+let str = "Web Developer";
+let index = 0;
+
+function print(){
+      if(index<str.length){
+        prof.innerHTML = prof.innerHTML+str[index];
+      index++;
+      setTimeout(print,150);
+     }else{
+    setTimeout(dlt,300);
+     }
+      
+}
+print();
+
+function dlt(){
+     if(index>0){
+      prof.innerHTML = str.substring(0,index-1);
+      index--;
+      setTimeout(dlt,100);
+     }
+     else{
+        setTimeout(print,100);
+     }
+}
